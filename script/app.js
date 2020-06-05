362081a25799cc11dcabfe0fc917866fd7baae31
Lukaszwolsza <luasply@gmail.com>
2020-06-05 22:23:00 +0200

    make some UI content, starting to make it works

--- script/app.js
+++ script/app.js
@@ -1,1 +1,20 @@
-console.log('Hello world !');
+const infoButton = document.getElementById('project-info-btn');
+const backdrop = document.querySelector('.backdrop');
+
+const projectName = document.querySelector('.projectName');
+const descInfo = document.querySelector('.description');
+
+const displayBox = document.getElementById('info-box-after-click');
+
+const makeBackdropVisible = () =>{
+    backdrop.classList.toggle('visible');
+    let projectNameBox = document.querySelector('.projectNameBackdrop');
+    let descBox = document.querySelector('.descBox');
+    projectNameBox.textContent = projectName;
+    descBox.textContent = descInfo;
+    displayBox.className.toggle('visible');
+}
+
+
+infoButton.addEventListener('click', makeBackdropVisible);
+
