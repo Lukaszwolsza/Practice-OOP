dac7ffdca1d0330b4fe50b5d2b65ed0640134420
Lukaszwolsza <luasply@gmail.com>
2020-06-07 09:11:47 +0200

    added modal

--- script/app.js
+++ script/app.js
@@ -1,1 +1,16 @@
+const infoBtn = document.getElementsByClassName('infoBtn');
+const modalBox = document.querySelector('.modal-bg');
+const closeModalBox = document.querySelector('.modal-btn');
 
+
+
+const dropdownBackground = () =>{
+    modalBox.classList.toggle('visible');
+}
+const dropdownBackgroundOff = () =>{
+    modalBox.classList.toggle('visible');
+}
+for(let i = 0; i < infoBtn.length; i++){
+    infoBtn[i].addEventListener('click', dropdownBackground);
+}
+closeModalBox.addEventListener('click', dropdownBackgroundOff);
